50e6195237e1cb8ba92dd39d5ea01c0d78149626
damascenodiego <damascenodiego@usp.br>
2019-10-29 22:44:01 +0000

    emse_prtz with k=0.4

--- FFSM_diff/Benchmark_SPL/emse_prtz.py
+++ FFSM_diff/Benchmark_SPL/emse_prtz.py
@@ -20,11 +20,12 @@ out_directory=re.sub("\.txt$","/",sys.argv[1])
 if not os.path.exists(out_directory):
 	os.makedirs(out_directory)
 
-
+_ktr=""
+_ktr="-t 0.40"
 _writer = open(out_directory+"/report.log","w")
 
 MODEL_ID=1
-bashCommand = "java -jar ./learnFFSM.jar -fm ./"+SPL_NAME+"/model.xml -mref "+prodAnalyzed[0]+" -updt "+prodAnalyzed[1]+" -out "+out_directory+"ffsm_"+str(MODEL_ID)+"_kiss.txt"
+bashCommand = "java -jar ./learnFFSM.jar "+_ktr+" -fm ./"+SPL_NAME+"/model.xml -mref "+prodAnalyzed[0]+" -updt "+prodAnalyzed[1]+" -out "+out_directory+"ffsm_"+str(MODEL_ID)+"_kiss.txt"
 _writer.write(bashCommand)
 _writer.write("\n")
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
@@ -42,7 +43,7 @@ for NEXT_ID in prodOrder:
 	MODEL_ID+=1
 	tmp=list(prodAnalyzed)
 	tmp.append(NEXT_ID)
-	bashCommand = "java -jar ./learnFFSM.jar -fm ./"+SPL_NAME+"/model.xml -fref "+out_directory+"ffsm_"+str(MODEL_ID-1)+"_kiss.txt  -updt "+NEXT_ID+" -out "+out_directory+"ffsm_"+str(MODEL_ID)+"_kiss.txt"
+	bashCommand = "java -jar ./learnFFSM.jar "+_ktr+" -fm ./"+SPL_NAME+"/model.xml -fref "+out_directory+"ffsm_"+str(MODEL_ID-1)+"_kiss.txt  -updt "+NEXT_ID+" -out "+out_directory+"ffsm_"+str(MODEL_ID)+"_kiss.txt"
 	_writer.write(bashCommand)
 	_writer.write("\n")
 	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
@@ -53,6 +54,20 @@ for NEXT_ID in prodOrder:
 	bashCommand = "java -jar ./learnFFSM.jar -fm ./"+SPL_NAME+"/model.xml -clean "+out_directory+"ffsm_"+str(MODEL_ID)+"_kiss.txt"
 	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 	output, error = process.communicate()
+	_writer.write(output)
+	_writer.write("\n")
+_writer.close()
+	
+
+_writer = open(out_directory+"/report_fmeasure.log","w")
+MAX_ID = MODEL_ID
+for MODEL_ID in range(1,MAX_ID+1):
+	# bashCommand = "java -cp ./learnFFSM.jar uk.le.ac.compare.CompareLanguages "+SPL_NAME+"/ffsms/ffsm_"+SPL_NAME+".txt "+out_directory+"ffsm_"+str(MODEL_ID)+"_kiss.txt "+_ktr+" -fm ./"+SPL_NAME+"/model.xml"
+	bashCommand = "java -cp ./learnFFSM.jar uk.le.ac.compare.CompareLanguages "+out_directory+"ffsm_"+str(MAX_ID)+"_kiss.txt "+out_directory+"ffsm_"+str(MODEL_ID)+"_kiss.txt "+_ktr+" -fm ./"+SPL_NAME+"/model.xml"
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+	_writer.write(bashCommand)
+	_writer.write("\n")
 	_writer.write(output)
 	_writer.write("\n")
 	prodAnalyzed=tmp
