146b2a92daadd685926ac303ab29f216561a426b
damascenodiego <damascenodiego@usp.br>
2019-10-30 19:38:53 +0000

    aerouc5 analysis removed from these scripts

--- FFSM_diff/Benchmark_SPL/emse_00setup.sh
+++ FFSM_diff/Benchmark_SPL/emse_00setup.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-for idx in {00..00}; do
-   for an_spl in "agm" "vm" "ws" "bcs2"; do
-      mkdir -p $an_spl/emse/recover
-      # ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml  -gmdp 2> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.txt
-      ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml        2> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.txt
-      # ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml  -gmdp -similar 2> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.txt
-      ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml        -similar 2> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.txt
-      for a_prtzd in $an_spl/emse/recover/[gl]mdp_*_$an_spl.txt; do
-         java -cp learnFFSM.jar uk.le.ac.prioritize.CalculateDissimilarity -fm $an_spl/model.xml -order $a_prtzd > $a_prtzd.score
-      done    
-   done
 
-   for an_spl in "aerouc5" "cpterminal" "minepump"; do
-      mkdir -p $an_spl/emse/recover
-      tar -xvf $an_spl/products.tar.gz -C $an_spl/
-      # ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml  -gmdp 2> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.txt
-      ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml        2> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.txt
-      # ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml  -gmdp -similar 2> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.txt
-      ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml        -similar 2> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.txt
-      for a_prtzd in $an_spl/emse/recover/[gl]mdp_*_$an_spl.txt; do
-         java -cp learnFFSM.jar uk.le.ac.prioritize.CalculateDissimilarity -fm $an_spl/model.xml -order $a_prtzd > $a_prtzd.score
-      done
+for an_spl in "agm" "vm" "ws" "bcs2"; do
+   mkdir -p $an_spl/emse/recover
+   for idx in {01..30}; do
+      # ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -gmdp 2> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.txt
+      ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle 2> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.txt
+      # ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -gmdp -similar 2> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.txt
+      ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -similar 2> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.txt
    done
+   # for a_prtzd in $an_spl/emse/recover/[gl]mdp_*_$an_spl.txt; do
+   #    java -cp learnFFSM.jar uk.le.ac.prioritize.CalculateDissimilarity -fm $an_spl/model.xml -order $a_prtzd > $a_prtzd.score
+   # done    
 done
+
+for an_spl in "cpterminal" "minepump"; do
+# for an_spl in "aerouc5" "cpterminal" "minepump"; do
+   mkdir -p $an_spl/emse/recover
+   tar -xvf $an_spl/products.tar.gz -C $an_spl/
+   for idx in {01..30}; do
+      # ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -gmdp 2> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.txt
+      ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle 2> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.txt
+      # ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -gmdp -similar 2> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.txt
+      ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -similar 2> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.txt
+   done
+   # for a_prtzd in $an_spl/emse/recover/[gl]mdp_*_$an_spl.txt; do
+   #    java -cp learnFFSM.jar uk.le.ac.prioritize.CalculateDissimilarity -fm $an_spl/model.xml -order $a_prtzd > $a_prtzd.score
+   # done
+done
+
 
 # # fix path: from UoL to Euler
 # sed -i 's/.var.autofs.home.home.cdnd1.git.learningFFSM.FFSM_diff/\/home\/damascdn\/remote_euler/g' */emse/recover/[lg]mdp*.txt
--- FFSM_diff/Benchmark_SPL/emse_01dissim.sh
+++ FFSM_diff/Benchmark_SPL/emse_01dissim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for an_spl in "agm" "vm" "ws" "aerouc5" "bcs2" "cpterminal" "minepump";
+for an_spl in "agm" "vm" "ws" "bcs2" "cpterminal" "minepump";
+# for an_spl in "agm" "vm" "ws" "aerouc5" "bcs2" "cpterminal" "minepump";
 do
    qsub -N "d_$an_spl" -v "name=emse_dissim_$an_spl" ./emse.sh
 done
