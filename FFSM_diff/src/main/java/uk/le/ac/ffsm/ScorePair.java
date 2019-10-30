a3017cbcc3a22a1d016ae06d551b7fe40420cae2
Carlos Diego Damasceno <damascenodiego@usp.br>
2019-10-30 00:35:21 +0000

    scorepair sortable by score>i>j

--- FFSM_diff/src/main/java/uk/le/ac/ffsm/ScorePair.java
+++ FFSM_diff/src/main/java/uk/le/ac/ffsm/ScorePair.java
@@ -1,5 +1,7 @@
 package uk.le.ac.ffsm;
 
+import net.automatalib.commons.util.nid.MutableNumericID;
+
 public class ScorePair<Item> implements Comparable<ScorePair>{
 	
 	private Double score;
@@ -28,7 +30,15 @@ public class ScorePair<Item> implements Comparable<ScorePair>{
 
 	@Override
 	public int compareTo(ScorePair o) {
-		return Double.compare(this.score, o.score);
+		int comp = Double.compare(this.score, o.score);
+		if(comp != 0) return comp;
+		if(this.statei instanceof MutableNumericID) {
+			comp = Integer.compare(((MutableNumericID) this.statei).getId(), ((MutableNumericID)o.getStatei()).getId());
+			if(comp != 0) return comp;
+			comp = Integer.compare(((MutableNumericID) this.statej).getId(), ((MutableNumericID)o.getStatej()).getId());
+			if(comp != 0) return comp;
+		}
+		return comp;
 	}
 	
 	@Override
