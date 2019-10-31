d6f144643a7ac27714f81703029c82b447be49ee
damascenodiego <damascenodiego@usp.br>
2019-10-31 12:50:11 +0000

    apfd analysis

--- FFSM_diff/Benchmark_SPL/emse_00setup.sh
+++ FFSM_diff/Benchmark_SPL/emse_00setup.sh
@@ -3,11 +3,12 @@
 
 for an_spl in "agm" "vm" "ws" "bcs2"; do
    mkdir -p $an_spl/emse/recover
-   for idx in {01..30}; do
-      # ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -gmdp 2> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.txt
+   for idx in {00..99}; do
+      ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -gmdp 2> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.txt
       ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle 2> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.txt
+      ls -1 $an_spl/fsm/fsm_$an_spl*txt | shuffle > $an_spl/emse/recover/rndp_dis_"$idx"_$an_spl.txt
       # ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -gmdp -similar 2> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.txt
-      ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -similar 2> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.txt
+      # ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -similar 2> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.txt
    done
    # for a_prtzd in $an_spl/emse/recover/[gl]mdp_*_$an_spl.txt; do
    #    java -cp learnFFSM.jar uk.le.ac.prioritize.CalculateDissimilarity -fm $an_spl/model.xml -order $a_prtzd > $a_prtzd.score
@@ -18,11 +19,12 @@ for an_spl in "cpterminal" "minepump"; do
 # for an_spl in "aerouc5" "cpterminal" "minepump"; do
    mkdir -p $an_spl/emse/recover
    tar -xvf $an_spl/products.tar.gz -C $an_spl/
-   for idx in {01..30}; do
-      # ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -gmdp 2> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.txt
+   for idx in {00..99}; do
+      ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -gmdp 2> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_dis_"$idx"_$an_spl.txt
       ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle 2> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_dis_"$idx"_$an_spl.txt
+      ls -1 $an_spl/products/products_all/*_kiss.txt | shuffle > $an_spl/emse/recover/rndp_dis_"$idx"_$an_spl.txt
       # ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -gmdp -similar 2> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/gmdp_sim_"$idx"_$an_spl.txt
-      ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -similar 2> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.txt
+      # ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -shuffle -similar 2> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.err 1> $an_spl/emse/recover/lmdp_sim_"$idx"_$an_spl.txt
    done
    # for a_prtzd in $an_spl/emse/recover/[gl]mdp_*_$an_spl.txt; do
    #    java -cp learnFFSM.jar uk.le.ac.prioritize.CalculateDissimilarity -fm $an_spl/model.xml -order $a_prtzd > $a_prtzd.score
--- FFSM_diff/Benchmark_SPL/emse_04logs2tab.sh
+++ FFSM_diff/Benchmark_SPL/emse_04logs2tab.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-for an_spl in "agm" "vm" "ws" "aerouc5" "bcs2" "cpterminal" "minepump";
-do
- echo "Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/StatesFFSM:Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/StatesFFSM" > ./pair_merging_$an_spl.log
- grep "^Reference" ./pair_merging_$an_spl.txt >> ./pair_merging_$an_spl.log
- sed -i 's/Reference\/Updated\/TotalStatesRef\/TotalStatesUpdt\/TotalFeaturesRef\/TotalFeaturesUpdt\/CommonFeatures\/RatioFeatures\/RatioStates\/StatesFFSM://g' ./pair_merging_$an_spl.log
- echo -e "Pair dissimilarity:Reference\tUpdated\tConfigDissim" > ./pair_dissimilarity_$an_spl.log
- grep "^Pair dissimilarity:" ./pair_dissimilarity_$an_spl.txt >> ./pair_dissimilarity_$an_spl.log
- sed -i 's/Pair dissimilarity://g' ./pair_dissimilarity_$an_spl.log
+# for an_spl in "agm" "vm" "ws" "aerouc5" "bcs2" "cpterminal" "minepump";
+# do
+#  echo "Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/StatesFFSM:Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/StatesFFSM" > ./pair_merging_$an_spl.log
+#  grep "^Reference" ./pair_merging_$an_spl.txt >> ./pair_merging_$an_spl.log
+#  sed -i 's/Reference\/Updated\/TotalStatesRef\/TotalStatesUpdt\/TotalFeaturesRef\/TotalFeaturesUpdt\/CommonFeatures\/RatioFeatures\/RatioStates\/StatesFFSM://g' ./pair_merging_$an_spl.log
+#  echo -e "Pair dissimilarity:Reference\tUpdated\tConfigDissim" > ./pair_dissimilarity_$an_spl.log
+#  grep "^Pair dissimilarity:" ./pair_dissimilarity_$an_spl.txt >> ./pair_dissimilarity_$an_spl.log
+#  sed -i 's/Pair dissimilarity://g' ./pair_dissimilarity_$an_spl.log
+# done
+
+
+for i in */emse/recover/[gld][nm]dp*.txt; do
+   echo "Starting $i";
+   qsub -N "$i" -v "name=$i" ./emse_03prtz.sh  ; 
+   while [ `qstat -u damascdn | wc -l` -gt 23 ]; do 
+      sleep 5s;
+   done; 
+done;
+
+for fname in */emse/recover/[gld][nm]dp*/report_fmeasure.log; do
+   cp  $fname `echo $fname | cut -d\/ -f4`.txt
+done
+
+
+for i in [gld][nm]dp*.log; do
+   echo "Precision|Recall|F-measure:Precision|Recall|F-measure" > $i.tab
+   grep "Precision|Recall|F-measure" $i >> $i.tab
+   sed -i "s/^Precision|Recall|F-measure://g" $i.tab
+done
+
+for i in */emse/recover; do
+   zip -rm $i.zip $i & 
 done
