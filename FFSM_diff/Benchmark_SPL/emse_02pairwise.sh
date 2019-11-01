68d8218ffcd72d16437e543e2b6cf43f73922bdb
damascenodiego <damascenodiego@usp.br>
2019-11-01 16:46:18 +0000

    cpterminal and minepump included

--- FFSM_diff/Benchmark_SPL/emse_02pairwise.sh
+++ FFSM_diff/Benchmark_SPL/emse_02pairwise.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for an_spl in "agm" "vm" "ws" "bcs2";
+# for an_spl in "agm" "vm" "ws" "bcs2";
 # for an_spl in "cpterminal" "minepump";
-# for an_spl in "agm" "vm" "ws" "bcs2" "cpterminal" "minepump";
+for an_spl in "agm" "vm" "ws" "bcs2" "cpterminal" "minepump";
 # for an_spl in "agm" "vm" "ws" "aerouc5" "bcs2" "cpterminal" "minepump";
 do
    qsub -N "p_$an_spl" -v "name=emse_pairs_$an_spl" ./emse.sh
