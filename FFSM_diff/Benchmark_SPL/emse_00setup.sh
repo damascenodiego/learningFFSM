ab30ce4fc29d7d7b450ef99a479524b20dedf9ea
damascenodiego <damascenodiego@usp.br>
2019-11-01 17:31:42 +0000

    30 rnd prtz added

--- FFSM_diff/Benchmark_SPL/emse_00setup.sh
+++ FFSM_diff/Benchmark_SPL/emse_00setup.sh
@@ -7,9 +7,9 @@ for an_spl in "agm" "vm" "ws" "bcs2"; do
    ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml                2> $an_spl/emse/recover/lmdp_dis_$an_spl.err 1> $an_spl/emse/recover/lmdp_dis_$an_spl.txt
    ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -gmdp -similar 2> $an_spl/emse/recover/gmdp_sim_$an_spl.err 1> $an_spl/emse/recover/gmdp_sim_$an_spl.txt
    ls -1 $an_spl/fsm/fsm_$an_spl*txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml       -similar 2> $an_spl/emse/recover/lmdp_sim_$an_spl.err 1> $an_spl/emse/recover/lmdp_sim_$an_spl.txt
-   # for idx in {00..99}; do
-   #    ls -1 $an_spl/fsm/fsm_$an_spl*txt | shuf > $an_spl/emse/recover/rndp_dis_"$idx"_$an_spl.txt
-   # done
+   for idx in {00..29}; do
+      ls -1 $an_spl/fsm/fsm_$an_spl*txt | shuf > $an_spl/emse/recover/rndp_dis_"$idx"_$an_spl.txt
+   done
 done
 
 # for an_spl in "cpterminal" "minepump"; do
@@ -20,7 +20,7 @@ for an_spl in "aerouc5" "cpterminal" "minepump"; do
    ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml                2> $an_spl/emse/recover/lmdp_dis_$an_spl.err 1> $an_spl/emse/recover/lmdp_dis_$an_spl.txt
    ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml -gmdp -similar 2> $an_spl/emse/recover/gmdp_sim_$an_spl.err 1> $an_spl/emse/recover/gmdp_sim_$an_spl.txt
    ls -1 $an_spl/products/products_all/*_kiss.txt | java -cp learnFFSM.jar uk.le.ac.prioritize.PrtzProducts -fm $an_spl/model.xml       -similar 2> $an_spl/emse/recover/lmdp_sim_$an_spl.err 1> $an_spl/emse/recover/lmdp_sim_$an_spl.txt
-   # for idx in {00..99}; do
-   #    ls -1 $an_spl/products/products_all/*_kiss.txt | shuf > $an_spl/emse/recover/rndp_dis_"$idx"_$an_spl.txt
-   # done
+   for idx in {00..29}; do
+      ls -1 $an_spl/products/products_all/*_kiss.txt | shuf > $an_spl/emse/recover/rndp_dis_"$idx"_$an_spl.txt
+   done
 done
