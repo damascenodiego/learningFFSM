7768bcca6b3dbc39e544af60fcf702c301fac77c
damascenodiego <damascenodiego@usp.br>
2019-11-05 17:32:50 +0000

    emse exp {31..99}

--- FFSM_diff/Benchmark_SPL/emse04logs2tab.sh
+++ FFSM_diff/Benchmark_SPL/emse04logs2tab.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd ~/remote_euler/
-mv ~/remote_euler/Benchmark_SPL/ ~/remote_euler/xxx2
-rm -rf ~/remote_euler/xxx2/ &
+mv ~/remote_euler/Benchmark_SPL/ ~/remote_euler/xxx
+rm -rf ~/remote_euler/xxx/ &
 cd ~/git/learningFFSM/
 git reset --hard
 git clean -fd
@@ -13,8 +13,8 @@ cd ~/remote_euler/Benchmark_SPL
 
 
 ./emse00setup.sh
-./emse01dissim.sh &
-./emse02pairwise.sh &
+# ./emse01dissim.sh &
+# ./emse02pairwise.sh &
 
 
 for i in */emse/recover/[glr][nm]dp*.txt; do
@@ -30,26 +30,26 @@ while [ `qstat -u damascdn | wc -l` -gt 1 ]; do
 sleep 5s;
 done;    
 
-mkdir ./exp_emse/pair/
-# for an_spl in "agm" "bcs2";
-# for an_spl in "agm" "vm" "ws" "bcs2";
-# for an_spl in "agm" "vm" "ws" "bcs2" "cpterminal" "minepump";
+# mkdir ./exp_emse/pair/
+# # for an_spl in "agm" "bcs2";
+# # for an_spl in "agm" "vm" "ws" "bcs2";
+# # for an_spl in "agm" "vm" "ws" "bcs2" "cpterminal" "minepump";
 # for an_spl in "agm" "vm" "ws" "bcs2" "cpterminal" "minepump" "aerouc5";
-for an_spl in "aerouc5";
-do
- echo "Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalTransitionsRef/TotalTransitionsUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/RatioTransitions/StatesFFSM/TransitionsFFSM:Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalTransitionsRef/TotalTransitionsUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/RatioTransitions/StatesFFSM/TransitionsFFSM" > ./exp_emse/pair/pair_merging_$an_spl.log
- grep "^Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalTransitionsRef/TotalTransitionsUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/RatioTransitions/StatesFFSM/TransitionsFFSM" ./$an_spl/emse/pair_merging_$an_spl.txt >> ./exp_emse/pair/pair_merging_$an_spl.log
- sed -i 's/Reference\/Updated\/TotalStatesRef\/TotalStatesUpdt\/TotalTransitionsRef\/TotalTransitionsUpdt\/TotalFeaturesRef\/TotalFeaturesUpdt\/CommonFeatures\/RatioFeatures\/RatioStates\/RatioTransitions\/StatesFFSM\/TransitionsFFSM://g' ./exp_emse/pair/pair_merging_$an_spl.log
+# # for an_spl in "aerouc5";
+# do
+#  echo "Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalTransitionsRef/TotalTransitionsUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/RatioTransitions/StatesFFSM/TransitionsFFSM:Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalTransitionsRef/TotalTransitionsUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/RatioTransitions/StatesFFSM/TransitionsFFSM" > ./exp_emse/pair/pair_merging_$an_spl.log
+#  grep "^Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalTransitionsRef/TotalTransitionsUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/RatioTransitions/StatesFFSM/TransitionsFFSM" ./$an_spl/emse/pair_merging_$an_spl.txt >> ./exp_emse/pair/pair_merging_$an_spl.log
+#  sed -i 's/Reference\/Updated\/TotalStatesRef\/TotalStatesUpdt\/TotalTransitionsRef\/TotalTransitionsUpdt\/TotalFeaturesRef\/TotalFeaturesUpdt\/CommonFeatures\/RatioFeatures\/RatioStates\/RatioTransitions\/StatesFFSM\/TransitionsFFSM://g' ./exp_emse/pair/pair_merging_$an_spl.log
  
- echo -e "Pair dissimilarity:Reference\tUpdated\tConfigDissim" > ./exp_emse/pair/pair_dissimilarity_$an_spl.log
- grep "^Pair dissimilarity:" ./$an_spl/emse/pair_dissimilarity_$an_spl.txt >> ./exp_emse/pair/pair_dissimilarity_$an_spl.log
- sed -i 's/Pair dissimilarity://g' ./exp_emse/pair/pair_dissimilarity_$an_spl.log
+#  echo -e "Pair dissimilarity:Reference\tUpdated\tConfigDissim" > ./exp_emse/pair/pair_dissimilarity_$an_spl.log
+#  grep "^Pair dissimilarity:" ./$an_spl/emse/pair_dissimilarity_$an_spl.txt >> ./exp_emse/pair/pair_dissimilarity_$an_spl.log
+#  sed -i 's/Pair dissimilarity://g' ./exp_emse/pair/pair_dissimilarity_$an_spl.log
 
- echo -e "ModelRef|ModelUpdt|Precision|Recall|F-measure:Reference|Updated|Precision|Recall|F-measure" > ./exp_emse/pair/pair_comparelanguages_$an_spl.log
- grep "^ModelRef|ModelUpdt|Precision|Recall|F-measure:" ./$an_spl/emse/pair_comparelanguages_$an_spl.txt >> ./exp_emse/pair/pair_comparelanguages_$an_spl.log
- sed -i 's/ModelRef|ModelUpdt|Precision|Recall|F-measure://g' ./exp_emse/pair/pair_comparelanguages_$an_spl.log
+#  echo -e "ModelRef|ModelUpdt|Precision|Recall|F-measure:Reference|Updated|Precision|Recall|F-measure" > ./exp_emse/pair/pair_comparelanguages_$an_spl.log
+#  grep "^ModelRef|ModelUpdt|Precision|Recall|F-measure:" ./$an_spl/emse/pair_comparelanguages_$an_spl.txt >> ./exp_emse/pair/pair_comparelanguages_$an_spl.log
+#  sed -i 's/ModelRef|ModelUpdt|Precision|Recall|F-measure://g' ./exp_emse/pair/pair_comparelanguages_$an_spl.log
 
-done
+# done
 
 
 
@@ -61,16 +61,16 @@ done
 
 for fname in ./exp_emse/prtz/[glr][nm]dp*_r.txt; do
    dest_file=$fname.log
-
    echo "Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalTransitionsRef/TotalTransitionsUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/RatioTransitions/StatesFFSM/TransitionsFFSM:Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalTransitionsRef/TotalTransitionsUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/RatioTransitions/StatesFFSM/TransitionsFFSM" > $dest_file
    grep "^Reference/Updated/TotalStatesRef/TotalStatesUpdt/TotalTransitionsRef/TotalTransitionsUpdt/TotalFeaturesRef/TotalFeaturesUpdt/CommonFeatures/RatioFeatures/RatioStates/RatioTransitions/StatesFFSM/TransitionsFFSM" $fname >> $dest_file
    sed -i 's/Reference\/Updated\/TotalStatesRef\/TotalStatesUpdt\/TotalTransitionsRef\/TotalTransitionsUpdt\/TotalFeaturesRef\/TotalFeaturesUpdt\/CommonFeatures\/RatioFeatures\/RatioStates\/RatioTransitions\/StatesFFSM\/TransitionsFFSM://g' $dest_file
-
 done
 
 
+cd exp_emse
+zip -rm prtz.zip prtz/* & 
+#zip -rm pair.zip pair/* & 
 
-for i in */emse/; do
-   zip -rm $i/recover.zip $i/recover/* & 
-   zip -rm $i/pair.zip $i/pair/* & 
-done
+cd ..
+
+zip -rm ~/emse.zip */emse/[lr]*
