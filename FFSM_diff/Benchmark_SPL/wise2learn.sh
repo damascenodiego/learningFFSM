8ed5ba96c340a881d637de5c8c1a52173e586977
damascenodiego <damascenodiego@usp.br>
2019-11-07 00:38:43 +0000

    SPLs with fts need preprocessing (fixed)

--- FFSM_diff/Benchmark_SPL/wise2learn.sh
+++ FFSM_diff/Benchmark_SPL/wise2learn.sh
@@ -7,7 +7,17 @@
 # 	qsub -N "p_$an_spl" -v "name=emse_pairs_$an_spl" ./emse.sh
 # done
 
-for an_spl in "agm" "vm" "ws" "cpterminal" "minepump" "aerouc5"; do
+for an_spl in "agm" "vm" "ws" "cpterminal"; do
+	cd $an_spl/
+	for a_prtz in ./products_*.prtz;do
+		python ../emse_prtz.py $a_prtz &
+	done
+	wait
+	cd -
+done
+
+
+for an_spl in "minepump" "aerouc5"; do
 	cd $an_spl/
 	for i in ./products_*/*.config; do 
 		java -cp ../learnFFSM.jar uk.le.ac.fts.FsmFromFTS -fts fts/*.fts -conf $i;
