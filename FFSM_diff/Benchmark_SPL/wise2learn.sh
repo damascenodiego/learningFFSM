84379bb82a76b90ebff69598a64aa35a3192745c
Carlos Diego Damasceno <damascenodiego@usp.br>
2019-11-07 01:51:46 +0000

    echo status

--- FFSM_diff/Benchmark_SPL/wise2learn.sh
+++ FFSM_diff/Benchmark_SPL/wise2learn.sh
@@ -7,16 +7,6 @@
 # 	qsub -N "p_$an_spl" -v "name=emse_pairs_$an_spl" ./emse.sh
 # done
 
-for an_spl in "agm" "vm" "ws" "cpterminal"; do
-	cd $an_spl/
-	for a_prtz in ./products_*.prtz;do
-		python ../emse_prtz.py $a_prtz &
-	done
-	wait
-	cd -
-done
-
-
 for an_spl in "minepump" "aerouc5"; do
 	cd $an_spl/
 	for i in ./products_*/*.config; do 
@@ -24,7 +14,18 @@ for an_spl in "minepump" "aerouc5"; do
 	done
 	for a_prtz in ./products_*.prtz;do
 		echo $an_spl" - "$a_prtz
-		python ../emse_prtz.py $a_prtz &
+		python ../emse_prtz.py $a_prtz
+	done
+	wait
+	cd -
+done
+
+
+for an_spl in "agm" "vm" "ws" "cpterminal"; do
+	cd $an_spl/
+	for a_prtz in ./products_*.prtz;do
+		echo $an_spl" - "$a_prtz
+		python ../emse_prtz.py $a_prtz
 	done
 	wait
 	cd -
