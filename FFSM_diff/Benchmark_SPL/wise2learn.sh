51df02e4be9ffbaa8be796685d6fd0c95535dfa3
Carlos Diego Damasceno <damascenodiego@usp.br>
2019-11-07 02:06:50 +0000

    wise2learn cpterminal moved to right for-loop

--- FFSM_diff/Benchmark_SPL/wise2learn.sh
+++ FFSM_diff/Benchmark_SPL/wise2learn.sh
@@ -3,11 +3,11 @@
 # ./emse01pairwise.sh
 
 
-# for an_spl in "agm" "vm" "ws" "cpterminal" "minepump" "aerouc5"; do
-# 	qsub -N "p_$an_spl" -v "name=emse_pairs_$an_spl" ./emse.sh
-# done
+for an_spl in "agm" "vm" "ws" "cpterminal" "minepump" "aerouc5"; do
+	qsub -N "p_$an_spl" -v "name=emse_pairs_$an_spl" ./emse.sh
+done
 
-for an_spl in "minepump" "aerouc5"; do
+for an_spl in "minepump" "aerouc5" "cpterminal"; do
 	cd $an_spl/
 	for i in ./products_*/*.config; do 
 		java -cp ../learnFFSM.jar uk.le.ac.fts.FsmFromFTS -fts fts/*.fts -conf $i;
@@ -21,7 +21,7 @@ for an_spl in "minepump" "aerouc5"; do
 done
 
 
-for an_spl in "agm" "vm" "ws" "cpterminal"; do
+for an_spl in "agm" "vm" "ws"; do
 	cd $an_spl/
 	for a_prtz in ./products_*.prtz;do
 		echo $an_spl" - "$a_prtz
